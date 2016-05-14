7ff6dc74c75ecf4ed90306328fb1dd5ef9461c24
Marcin Antolak <minantak@gmail.com>
2016-05-14 23:53:45 +0200

    Add source files
    Add first version of source files.

---
+++ MikroHotel/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MikroHotel.about;
+
+
+namespace MikroHotel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button4_OnClick(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            About_Window abtWindow = new About_Window();
+            abtWindow.Show();
+        }
+
+        private void Button5_OnClick(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            this.Close();
+        }
+    }
+    
+}
