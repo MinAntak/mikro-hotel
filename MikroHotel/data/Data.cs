ac6dd3186cbb8bda66525297793db8f9489cf6cd
Marcin Antolak <minantak@gmail.com>
2016-05-15 22:10:06 +0200

    Add data classes

---
+++ MikroHotel/data/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikroHotel.data
+{
+    public class Data
+    {
+        public int Room { get; set; }
+        protected int people; //how many people in room
+        protected String description;
+
+        public Data()
+        {
+            
+        }
+        
+
+    }
+}
---
+++ MikroHotel/data/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikroHotel.data
+{
+    public class Room : Data
+    {
+        private int id;
+        private int number;
+
+        public Room()
+        {
+            
+        }
+
+        public void randomID()
+        {
+            Random rand = new Random();
+
+            id = rand.Next();
+        }
+    }
+}
